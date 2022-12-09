2e9b7653f6c6fa8cce42cf1b970febd39df48ab1
Nikita <lavro.nikita123@gmail.com>
2022-12-08 22:04:28 -0500

    NoteCard complete JSX and CSS

--- note-ts-app/src/components/NoteCard/NoteCard.tsx
+++ note-ts-app/src/components/NoteCard/NoteCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 //Bootstrap
-import { Card } from "react-bootstrap";
+import { Card, Stack, Badge } from "react-bootstrap";
 
 //Router
 import { Link } from "react-router-dom";
@@ -10,16 +10,36 @@ import { Link } from "react-router-dom";
 import { SimplifiedNote } from "../../App";
 
 //Styles
-import styles from "./NoteCard.module.css";
+import "./NoteCard.css";
 
 const NoteCard = ({ id, title, tags }: SimplifiedNote) => {
   return (
     <Card
       as={Link}
       to={`/${id}`}
-      className={`h-100 text-reset text-decoration-none ${styles.card}`}
+      className={`h-100 text-reset text-decoration-none card`}
     >
-      <Card.Body></Card.Body>
+      <Card.Body>
+        <Stack
+          gap={2}
+          className="align-items-center justify-content-center h-100"
+        >
+          <span className="fs-5">{title}</span>
+          {tags.length > 0 && (
+            <Stack
+              gap={1}
+              direction="horizontal"
+              className="justify-content-center flex-wrap"
+            >
+              {tags.map((tag) => (
+                <Badge className="text-truncate" key={tag.id}>
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      </Card.Body>
     </Card>
   );
 };
