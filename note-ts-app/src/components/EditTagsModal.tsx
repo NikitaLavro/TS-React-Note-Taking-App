67794f4734920e733bc35f26966b0ec14a848309
Nikita <lavro.nikita123@gmail.com>
2022-12-14 13:13:38 -0500

    EditTagsModal component and JSX

---
+++ note-ts-app/src/components/EditTagsModal.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+//Bootstrap
+import { Modal, Form, Stack, Row, Col, Button } from "react-bootstrap";
+
+//TS interface
+import { Tag, Note } from "../App";
+
+interface EditTagsModalProps {
+  availableTags: Tag[];
+}
+
+const EditTagsModal = ({ availableTags }: EditTagsModalProps) => {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>Edit Tags</Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Stack gap={2}>
+            {availableTags.map((tag) => (
+              <Row key={tag.id}>
+                <Col>
+                  <Form.Control type="text" value={tag.label} />
+                </Col>
+                <Col xs="auto">
+                  <Button variant="outline-danger">&times;</Button>
+                </Col>
+              </Row>
+            ))}
+          </Stack>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default EditTagsModal;
